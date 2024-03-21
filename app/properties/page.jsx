79d5824ad816b96e03c7abcabe53e8bc8390af56
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-03-21 13:36:07 +0100

    committed new components PropertyCard, resituated images

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,22 @@
-const pageProperty = () => {
-  return <div>page for props</div>;
-};
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
-export default pageProperty;
+const PropertiesPage = () => {
+  return (
+    // <!-- All Listings -->
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <div className='bg-red-600'>No Property Found</div>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default PropertiesPage;
