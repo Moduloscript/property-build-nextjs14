ba1f16730a52ef0fa6ccd94890ce2fa4fc81aefb
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-03-24 07:45:45 +0100

    propertyDetailsPage was partly enhanced

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import PropertyDetails from "@/components/PropertyDetails";
+import {FaArrowLeft} from "react-icons/fa"
 
 const PropertyId = () => {
   const { id } = useParams();
@@ -37,7 +40,25 @@ const PropertyId = () => {
     <>
       {!loading && property && (
         <>
-          <PropertyHeaderImage image={property.images[0] } />
+          <PropertyHeaderImage image={property.images[0]} />
+
+          {/* <!-- Go Back --> */}
+          <section>
+            <div className='container m-auto py-6 px-6'>
+              <Link
+                href='/properties'
+                className='text-blue-500 hover:text-blue-600 flex items-center'>
+                <FaArrowLeft className="mr-2"/> Back to Properties
+              </Link>
+            </div>
+          </section>
+          <section className='bg-blue-50'>
+            <div className='container m-auto py-10 px-6'>
+              <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+                <PropertyDetails property={property} />
+              </div>
+            </div>
+          </section>
         </>
       )}
     </>
