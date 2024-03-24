209be53945249852d81f972d5ebef213f30b264a
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-03-24 09:02:19 +0100

    created an authProvider component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,24 @@
-
 import "@/assets/styles/globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Property Pulse",
   description: "Get the Best Property Rental in town",
-  keywords:"Property as the Best Rental in The country"
+  keywords: "Property as the Best Rental in The country",
 };
 
 export default function mainLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar/>
-        <main>{children}</main>
-        <Footer/>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
