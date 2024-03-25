6cec6de2851d8cd1d6ec68d82acf7af492fff8df
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-03-25 19:45:25 +0100

    added authentication on the Navbar component (intro steps)

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-import {FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker} from "react-icons/fa"
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from 'react-icons/fa';
 
 const PropertyCard = ({ property }) => {
+
   const {
     name,
     images,
@@ -16,6 +23,8 @@ const PropertyCard = ({ property }) => {
   } = property;
 
   const getRateDisplay = () => {
+    const { rates } = property;
+
     if (rates.monthly) {
       return `${rates.monthly.toLocaleString()}/mo`;
     } else if (rates.weekly) {
@@ -28,17 +37,17 @@ const PropertyCard = ({ property }) => {
   return (
     <div className='rounded-xl shadow-md relative'>
       <Image
-        src={`/images/properties/${images[0]}`}
-        alt={name}
-        sizes='100vw'
+        src = {`/images/properties/${images[0]}`}
+        alt=''
         height={0}
         width={0}
+        sizes='100vw'
         className='w-full h-auto rounded-t-xl'
       />
       <div className='p-4'>
         <div className='text-left md:text-center lg:text-left mb-6'>
-          <div className='text-gray-600'>{type}</div>
-          <h3 className='text-xl font-bold'>{name}</h3>
+          <div className='text-gray-600'>{property.type}</div>
+          <h3 className='text-xl font-bold'>{property.name}</h3>
         </div>
         <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
           ${getRateDisplay()}
@@ -46,59 +55,54 @@ const PropertyCard = ({ property }) => {
 
         <div className='flex justify-center gap-4 text-gray-500 mb-4'>
           <p>
-            <FaBed className="inline mr-2"/>
-            {beds} { " "} <span className='md:hidden lg:inline'>Beds</span>
+            <FaBed className='inline mr-2' /> {property.beds}{' '}
+            <span className='md:hidden lg:inline'>Beds</span>
           </p>
           <p>
-            <FaBath className="inline mr-2"/> {baths}{" "}
-            <span className='md:hidden lg:inline'>Baths</span>
+            <FaBath className='inline mr-2' />
+            {property.baths} <span className='md:hidden lg:inline'>Baths</span>
           </p>
           <p>
-          <FaRulerCombined className="inline mr-2"/>
-            {square_feet} <span className='md:hidden lg:inline'>sqft</span>
+            <FaRulerCombined className='inline mr-2' />
+            {property.square_feet}{' '}
+            <span className='md:hidden lg:inline'>sqft</span>
           </p>
         </div>
 
         <div className='flex justify-center gap-4 text-green-900 text-sm mb-4'>
-          <p>
-            {rates.nightly && (
-              <p>
-                <FaMoneyBill className="inline mr-2"/>
-                Nightly
-              </p>
-            )}
-          </p>
-          <p>
-            {rates.weekly && (
-              <p>
-              <FaMoneyBill className="inline mr-2"/>
-                Weekly
-              </p>
-            )}
-          </p>
-          <p>
-            {rates.monthly && (
-              <p>
-              <FaMoneyBill className="inline mr-2"/>
-                Monthly
-              </p>
-            )}
-          </p>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className='inline mr-2' /> Nightly
+            </p>
+          )}
+
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className='inline mr-2' /> Weekly
+            </p>
+          )}
+
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className='inline mr-2' /> Monthly
+            </p>
+          )}
         </div>
 
         <div className='border border-gray-100 mb-5'></div>
 
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='flex align-middle gap-2 mb-4 lg:mb-0'>
-            <FaMapMarker className="text-orange-700 mt-1"/>
+            <FaMapMarker className='text-orange-700 mt-1' />
             <span className='text-orange-700'>
-              {" "}
-              {location.city} {location.state}{" "}
+              {' '}
+              {property.location.city} {property.location.state}{' '}
             </span>
           </div>
           <Link
-            href={`/properties/${_id}`}
-            className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
+            href={`/properties/${property._id}`}
+            className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'
+          >
             Details
           </Link>
         </div>
@@ -106,5 +110,4 @@ const PropertyCard = ({ property }) => {
     </div>
   );
 };
-
 export default PropertyCard;
